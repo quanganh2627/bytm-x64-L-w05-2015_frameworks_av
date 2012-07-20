049f9b8471a07e614ac8e10ec9f1b2f44112b76b
ywan171 <yi.a.wang@intel.com>
2012-07-20 19:12:41 +0800

    libstagefright: enable httplive and rtsp in JB
    BZ:48555
    
    enable httplive and rtsp in JB,Set Intel OMX decoder Quirks kRequiresLoadedToIdleAfterAllocation
    to enalbe allocatbuffer after state change
    
    orig-patch
    31312:libstagefright: enable HTTPLive and RTSP on ICS 4.0.3
    
    Change-Id: I9dd2a599af43ecc22d97b59fc3b5662edd4368a3
    Signed-off-by: ywan171 <yi.a.wang@intel.com>
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Reviewed-on: http://android.intel.com:8080/76013
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Tong, BoX <box.tong@intel.com>

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -3345,11 +3345,11 @@ bool ACodec::LoadedState::onConfigureComponent(
 
 void ACodec::LoadedState::onStart() {
     ALOGV("onStart");
-
-    CHECK_EQ(mCodec->mOMX->sendCommand(
-                mCodec->mNode, OMX_CommandStateSet, OMX_StateIdle),
-             (status_t)OK);
-
+    if (!(mCodec->mQuirks & OMXCodec::kRequiresLoadedToIdleAfterAllocation)) {
+        CHECK_EQ(mCodec->mOMX->sendCommand(
+                    mCodec->mNode, OMX_CommandStateSet, OMX_StateIdle),
+                 (status_t)OK);
+    }
     mCodec->changeState(mCodec->mLoadedToIdleState);
 }
 
@@ -3371,6 +3371,10 @@ void ACodec::LoadedToIdleState::stateEntered() {
         mCodec->signalError(OMX_ErrorUndefined, err);
 
         mCodec->changeState(mCodec->mLoadedState);
+    } else if (mCodec->mQuirks & OMXCodec::kRequiresLoadedToIdleAfterAllocation) {
+        CHECK_EQ(mCodec->mOMX->sendCommand(
+                    mCodec->mNode, OMX_CommandStateSet, OMX_StateIdle),
+                 (status_t)OK);
     }
 }
 
