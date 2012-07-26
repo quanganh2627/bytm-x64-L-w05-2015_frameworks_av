e83d152d0f142e69a1d89e44dfbaf5bf4681a3b7
Jason Hu <jason.hu@intel.com>
2012-07-26 17:25:57 -0400

    [PORT FROM MAIN] To fix video flicking issue when rendering through overlay.
    BZ: 48923
    
    Currently, hw overlay need always hold an output buffer until flip
    to another buffer. So we should hold two more buffers in surface
    texture back end to avoid blocking on dequeue buffer by decoder.
    
    Orig-Change-Id: I63f64267879bd95383167a706aadad78f4fc118b
    Change-Id: I60c5483952a5c22bf008f259979f96ba50f292fe
    Signed-off-by: Jason Hu <jason.hu@intel.com>
    Reviewed-on: http://android.intel.com:8080/123507
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -106,6 +106,7 @@ struct OMXCodec : public MediaSource,
         kSupportsMultipleFramesPerInputBuffer = 1024,
         kRequiresLargerEncoderOutputBuffer    = 2048,
         kOutputBuffersAreUnreadable           = 4096,
+        kRequiresHoldExtraBuffers             = 65536,
         kRequiresSampleRate                   = 131072,
     };
 
