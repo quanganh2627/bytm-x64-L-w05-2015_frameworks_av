f5b88fa0785f86c5eb6c2ae5a8b8e650250cec63
gbhakteX <gurudattax.bhakte@intel.com>
2013-01-10 17:49:26 +0530

    [PORT FROM MAIN]Audio-MW:Klocwork Fix required in Stagefright engine and codec-layer.
    BZ: 78025
    
    Klocwork issue #1323: 'mode' might be used uninitialized in this function.
    Fix: Rearranged the code so that if getMode() returns "mode", then only it is
    used otherwise isInCall() returns false.
    [AwesomePlayer.cpp | isInCall() | line 3654].
    
    Klocwork issue #1328: 'err' might be used uninitialized in this function.
    Fix: Initialized 'err' with 'OK'.
    [AwesomePlayer.cpp | setSurfaceTexture() | line 1455].
    
    Klocwork issue #3564: Array 'mMetaBuffer' of size 6 may use index value(s) -1. Also
    there are 10 similar errors on line(s) 94, 99, 100, 101, 102, 103, 104, 107, 108, 111.
    Fix: Provided the check to return Error, to avoid 'mMetaBuffer[]' using index value -1.
    [FileSource.cpp | readFromBuffer() | 93].
    
    Klocwork issue #5846: Pointer 'gmtime( &time_1970)' returned from call to function
    'gmtime' at line 604 may be NULL and will be dereferenced at line 604.
    Fix: 'strftime()' is invoked only if 'gmtime()' returns a valid pointer to 'struct tm'
    which contains the time information.
    [MPEG4Extractor.cpp | convertTimeToDate() | line 604].
    
    Klocwork issue #5979: Pointer 'track' returned from call to function 'GetTrackByNumber'
    at line 465 may be NULL and will be dereferenced at line 472.
    Fix: Provided the check to return Error if pointer 'track' returned from call to function
    'GetTrackByNumber' is NULL.
    [MatroskaExtractor.cpp | readBlock() | line 472]
    
    Klocwork issue #6423: Possible memory leak. Dynamic memory stored in 'tmp' allocated
    through function 'new' at line 487 can be lost at line 512. Also there is one similar
    error on line 525.
    Fix: Dynamic memory stored in 'tmp' is released before returning Error to avoid possible
    memory leak.
    [OggExtractor.cpp | readNextPacket() | line 512, 529].
    
    Category: aosp improvement
    Domain: AUDIO_MEDIA-Framework
    Origin: Internal
    Upstream-Candidate: no
    
    Change-Id: I11085d66c5a27b75ba1be3ad2aa3abe28bed5941
    Orig-Change-Id: I4ca9f0796ea92ed25ea8351a7d9b4a285ccc8ce7
    Signed-off-by: gbhakteX <gurudattax.bhakte@intel.com>

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -736,7 +736,11 @@ static void convertTimeToDate(int64_t time_1904, String8 *s) {
     time_t time_1970 = time_1904 - (((66 * 365 + 17) * 24) * 3600);
 
     char tmp[32];
-    strftime(tmp, sizeof(tmp), "%Y%m%dT%H%M%S.000Z", gmtime(&time_1970));
+    struct tm *ptr_time;
+    ptr_time = gmtime(&time_1970);
+    if (ptr_time != NULL) {
+        strftime(tmp, sizeof(tmp), "%Y%m%dT%H%M%S.000Z", ptr_time);
+    }
 
     s->setTo(tmp);
 }
