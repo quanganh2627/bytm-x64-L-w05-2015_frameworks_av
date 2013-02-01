d1bfc6c8dc604c374348b35ba89a3a393e115e0d
Tang Guifang <guifang.tang@intel.com>
2013-02-01 04:14:48 -0500

    OMX: avoid illegal access to NULL pointer in callback.
    BZ: 81944
    
    When OMX client gone with exception, in binderDied function it will
    remove the corresponding node from mDispatchers array. Callback function
    will still go on even if the dispatcher is not found, and causes crash.
    Add NULL check to avoid such crash issue.
    
    Change-Id: I4ca5542d3cc2195cc4a6f270cafb23a49ba9b8fc
    Reviewed-on: http://android.intel.com:8080/90185
    Reviewed-by: Gu, Wangyi <wangyi.gu@intel.com>
    Reviewed-by: Tang, Guifang <guifang.tang@intel.com>
    Reviewed-by: Wang, Yi A <yi.a.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: Gozalvez Herrero, Juan AntonioX <juan.antoniox.gozalvez.herrero@intel.com>
    Tested-by: Gozalvez Herrero, Juan AntonioX <juan.antoniox.gozalvez.herrero@intel.com>

--- media/libstagefright/omx/OMX.cpp
+++ media/libstagefright/omx/OMX.cpp
@@ -400,7 +400,12 @@ OMX_ERRORTYPE OMX::OnEvent(
     msg.u.event_data.data1 = nData1;
     msg.u.event_data.data2 = nData2;
 
-    findDispatcher(node)->post(msg);
+    sp<OMX::CallbackDispatcher> cbd = findDispatcher(node);
+    if (cbd == NULL) {
+        ALOGE("CallbackDispatcher for this node is not found");
+        return OMX_ErrorUndefined;
+    }
+    cbd->post(msg);
 
     return OMX_ErrorNone;
 }
@@ -414,7 +419,12 @@ OMX_ERRORTYPE OMX::OnEmptyBufferDone(
     msg.node = node;
     msg.u.buffer_data.buffer = pBuffer;
 
-    findDispatcher(node)->post(msg);
+    sp<OMX::CallbackDispatcher> cbd = findDispatcher(node);
+    if (cbd == NULL) {
+        ALOGE("CallbackDispatcher for this node is not found");
+        return OMX_ErrorUndefined;
+    }
+    cbd->post(msg);
 
     return OMX_ErrorNone;
 }
@@ -434,7 +444,12 @@ OMX_ERRORTYPE OMX::OnFillBufferDone(
     msg.u.extended_buffer_data.platform_private = pBuffer->pPlatformPrivate;
     msg.u.extended_buffer_data.data_ptr = pBuffer->pBuffer;
 
-    findDispatcher(node)->post(msg);
+    sp<OMX::CallbackDispatcher> cbd = findDispatcher(node);
+    if (cbd == NULL) {
+        ALOGE("CallbackDispatcher for this node is not found");
+        return OMX_ErrorUndefined;
+    }
+    cbd->post(msg);
 
     return OMX_ErrorNone;
 }
