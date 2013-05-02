b8a6e1a823ab3160306c97e2428f2563ea8bc42c
ywan171 <yi.a.wang@intel.com>
2013-05-02 17:05:42 +0800

    [PORT FROM MAIN] libstagefright: enhance error handler in MPEG4EXtractor
    BZ: 92698
    
    Enhance error handler in MPEG4Extractor, there's case that nalLength > size
    but nalLength + 4 < size, for example nalLength = 0xffffffff.
    
    Change-Id: Idb9de12623f9afbd5356664ebf0ee79369c3123c
    Orig-Change-Id: I6036d34261ca8591679082c3a85c7e34ee42a393
    Category: aosp improvement
    Domain: Video.Media-container
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: ywan171 <yi.a.wang@intel.com>
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Signed-off-by: Yuanjun Huang <yuanjun.huang@intel.com>

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -3288,7 +3288,7 @@ status_t MPEG4Source::read(
                 if (!isMalFormed) {
                     nalLength = parseNALSize(&mSrcBuffer[srcOffset]);
                     srcOffset += mNALLengthSize;
-                    isMalFormed = srcOffset + nalLength > size;
+                    isMalFormed = nalLength > size | srcOffset + nalLength > size;
                 }
 
                 if (isMalFormed) {
