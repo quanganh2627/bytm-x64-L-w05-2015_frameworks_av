9e20813c8208cac0a89eac0b0a911395351139a8
Arulselvan M <arulselvan.m@intel.com>
2013-05-30 16:47:21 +0530

    [PORT FROM MAIN]MediaPlayerService: check buffer before set it's size to zero
    BZ: 69444
    
    Sometimes the output is null, then code set the buffer size to zero, but
    in some special case, the buffer(which is pass through info pointer) is
    null too, so check it before access the pointer.
    
    Change-Id: I39dd94a85922ce38425e0123dd1d1ca5d9798482
    Orig-Change-Id: Idfe5a07b374108de5e4ce5c4c8b6baee30fca652
    Signed-off-by: Arulselvan M <arulselvan.m@intel.com>

--- media/libmediaplayerservice/MediaPlayerService.cpp
+++ media/libmediaplayerservice/MediaPlayerService.cpp
@@ -1665,7 +1665,7 @@ void MediaPlayerService::AudioOutput::CallbackWrapper(
         // no output set, likely because the track was scheduled to be reused
         // by another player, but the format turned out to be incompatible.
         data->unlock();
-        buffer->size = 0;
+        if (buffer != NULL) buffer->size = 0;
         return;
     }
 
