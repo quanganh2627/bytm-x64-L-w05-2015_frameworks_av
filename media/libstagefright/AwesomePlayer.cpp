1b733fb816f8a8f2bdf98407de2e7cb2d42de33b
Lin Xie <lin.xie@intel.com>
2013-07-01 18:49:04 +0800

    [PORT FROM MAIN] Video-HDMI: Set video state only when video source is valid
    BZ: 120151
    
    When play audio only, an Awesomeplayer object is created but with
    no video source.
    The video state should not be set under such circumstance.
    
    Change-Id: I3dfee200a28ed50c15cd5623e21ae6774ca9f34b
    Orig-Change-Id: Id2d301d0b74b8780f6b5e688234e1b650d68f018
    Signed-off-by: Lin Xie <lin.xie@intel.com>
    Signed-off-by: Jason Hu <jason.hu@intel.com>
    Reviewed-on: http://android.intel.com:8080/122395
    Reviewed-by: Ding, Haitao <haitao.ding@intel.com>
    Tested-by: Ding, Haitao <haitao.ding@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -455,6 +455,7 @@ void AwesomePlayer::setDisplaySource_l(bool isplaying) {
                     info.displayH = mStats.mVideoHeight;
                 }
                 mMDClient->setVideoSourceInfo(&info);
+                notifyMDSPlayerStatus_l(MDS_VIDEO_PREPARED);
             }
         }
     } else {
@@ -1229,7 +1230,6 @@ status_t AwesomePlayer::play_l() {
     addBatteryData(params);
 #ifdef TARGET_HAS_MULTIPLE_DISPLAY
     setDisplaySource_l(true);
-    notifyMDSPlayerStatus_l(MDS_VIDEO_PREPARED);
 #endif
     return OK;
 }
