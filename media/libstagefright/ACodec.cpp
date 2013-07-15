1a8e3ff35def1a313bb01ca6e99705e930f8caf7
wanglili <lili.a.wang@intel.com>
2013-07-16 00:26:06 +0800

    Fix KW issues and refine error handling
    BZ: 123464 122111 125565
    
    Orignal patches:
    122525, BZ: 123464, Fix KW issues.
    122527, BZ: 122111, Refine VPP error handling in AwesomePlayer.
    124700, BZ: 125565, Use getInstance method to create VPPProcessor.
    124704, BZ: 127276, Reset mVPPInit flag when creating VPPProcessor.
    
    1. Fix KW issues.
    https://android-kw2.jf.intel.com:8090/review/insight-review.html#goto:project=mainline_merr_vv,pid=109923
    https://android-kw2.jf.intel.com:8090/review/insight-review.html#goto:project=mainline_baylake,pid=97445
    
    2. Refine VPP error handling in AwesomePlayer.
    Invoke VPPProcessor->updateVideoInfo() and VPPProcessor->init()
    explicitly, in order to handle error in a proper way.
    
    3. Since VPPProcessor works in single instance mode, and player should
    use getInstance API to create/query VPPProcessor.
    
    4. Reset mVPPInit flag when creating VPPProcessor.
    mVPPInit flag is not reset, which will cause mediaserver crash
    if replay.
    
    Category: feature differentiation
    Domain: Video.Media-VPP
    Origin:  internal
    Upstream-Candidate: No
    
    Change-Id: I217778c2514378a6347e42d32b5b642894809aa5
    Orig-Change-Id: Ia1f1a9ac61377d1c256bef1c43e35962f8e1c5b2
    Orig-Change-Id: I322116c912cc87dec800bc9c9bf925933526935e
    Orig-Change-Id: I8eff014fe52ebdb05084d0f9370628d2bc230476
    Orig-Change-Id: Ia09ff57b43695ced8756d8225e6b3273cc375309
    Signed-off-by: wanglili <lili.a.wang@intel.com>
    Signed-off-by: Yu, Linda <linda.yu@intel.com>
    Signed-off-by: Xigui Wang <xigui.wang@intel.com>

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -892,7 +892,7 @@ ACodec::BufferInfo *ACodec::dequeueBufferFromNativeWindow() {
         }
 
         size_t i;
-        BufferInfo *info;
+        BufferInfo *info = NULL;
         for (i = mBuffers[kPortIndexOutput].size(); i-- > 0;) {
             info = &mBuffers[kPortIndexOutput].editItemAt(i);
 
@@ -900,7 +900,7 @@ ACodec::BufferInfo *ACodec::dequeueBufferFromNativeWindow() {
                 break;
             }
         }
-        if (i < 0) return NULL;
+        if (i < 0 || info == NULL) return NULL;
         /*
          * The buffer is still used as reference by VPP,
          * go on dequeue next buffer, until we get a free one.
