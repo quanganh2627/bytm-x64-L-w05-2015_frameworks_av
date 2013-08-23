47e6ea61567f1bd1a2a310dea3f81f4ab41ee2ff
sandhya <sandhyax.sebastian@intel.com>
2013-08-23 17:18:25 +0530

    [PORT FROM MAIN][KW] Klocwork fix required in AudioFlinger modules
    BZ:132274
    
    Checks added to avoid Null pointer dereference.
    Initialization added for some class members in member initialization in
    constructor where it were not initialized.
    
    Severity: high
    
    Change-Id: Iadcd717480957443f1810e68c4e3dcf354c545f6
    Orig-Change-Id: I10df784fe88b1cbc08be936e9b5869778c9bc555
    Category: aosp improvement
    Domain: AUDIO_MEDIA - framework
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: sandhya <sandhyax.sebastian@intel.com>

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -912,7 +912,8 @@ AudioFlinger::EffectHandle::EffectHandle(const sp<EffectModule>& effect,
                                         int32_t priority)
     : BnEffect(),
     mEffect(effect), mEffectClient(effectClient), mClient(client), mCblk(NULL),
-    mPriority(priority), mHasControl(false), mEnabled(false), mDestroyed(false)
+    mBuffer(NULL), mPriority(priority), mHasControl(false), mEnabled(false),
+    mDestroyed(false)
 {
     ALOGV("constructor %p", this);
 
