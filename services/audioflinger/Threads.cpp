8aad21589f99c045e44adbeb6af97b653aabc4eb
sandhya <sandhyax.sebastian@intel.com>
2013-08-23 17:18:25 +0530

    [PORT FROM MAIN][KW] Klocwork fix required in AudioFlinger modules
    BZ:132274
    
    Checks added to avoid Null pointer dereference.
    Initialization added for some class members in member initialization in
    constructor where it were not initialized.
    
    Category: aosp improvement
    Domain: AUDIO_MEDIA - framework
    Severity: high
    Origin: internal
    Upstream-Candidate: no
    
    Change-Id: Iadcd717480957443f1810e68c4e3dcf354c545f6
    Orig-Change-Id: I10df784fe88b1cbc08be936e9b5869778c9bc555
    Signed-off-by: sandhya <sandhyax.sebastian@intel.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -2855,7 +2855,10 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::MixerThread::prepareTrac
             int j = track->mFastIndex;
             ALOG_ASSERT(0 < j && j < (int)FastMixerState::kMaxFastTracks);
             ALOG_ASSERT(!(mFastTrackAvailMask & (1 << j)));
-            FastTrack *fastTrack = &state->mFastTracks[j];
+            FastTrack *fastTrack;
+            if ( state != NULL ) {
+                fastTrack = &state->mFastTracks[j];
+            }
 
             // Determine whether the track is currently in underrun condition,
             // and whether it had a recent underrun.
@@ -2957,7 +2960,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::MixerThread::prepareTrac
 
             if (isActive) {
                 // was it previously inactive?
-                if (!(state->mTrackMask & (1 << j))) {
+                if ((state != NULL ) && (!(state->mTrackMask & (1 << j)))) {
                     ExtendedAudioBufferProvider *eabp = track;
                     VolumeProvider *vp = track;
                     fastTrack->mBufferProvider = eabp;
@@ -2975,7 +2978,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::MixerThread::prepareTrac
                 ++fastTracks;
             } else {
                 // was it previously active?
-                if (state->mTrackMask & (1 << j)) {
+                if ((state != NULL) && (state->mTrackMask & (1 << j))) {
                     fastTrack->mBufferProvider = NULL;
                     fastTrack->mGeneration++;
                     state->mTrackMask &= ~(1 << j);
