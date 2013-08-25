d6afa2c0c5313b3aee703bda1fea888fb8746e2e
guruprasadX, pawse <guruprasadx.pawse@intel.com>
2013-08-25 11:37:57 +0530

    [PORT FROM MAIN]ANR when playing H.264 video with AAC-LC content(selected erroneous clip)
    BZ: 129997
    
    For selected erroneous clip the MatroskaExtractor returns a negative
    blockTimeUs, due to this the check at OMXCodec.cpp fails and hence results
    into an ANR.
    
    This fix checks for the blockTimeUs in the MatroskaExtractor and exits with
    UNKNOWN_ERROR if the value is negative.
    
    Category: aosp improvement
    Domain: AUDIO_MEDIA - framework
    Severity: medium
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: Ie219f426143947e25480e0b624ddab7a877e119c
    Orig-Change-Id: Ibb877ed364ace68cbaf50040cce937f25fb62f62

--- media/libstagefright/matroska/MatroskaExtractor.cpp
+++ media/libstagefright/matroska/MatroskaExtractor.cpp
@@ -479,6 +479,10 @@ status_t MatroskaSource::readBlock() {
     const mkvparser::Track *track = tracks->GetTrackByNumber(block->GetTrackNumber());
     int64_t nextBlkTimeUs, diffTimeUs, frameTimeUs;
     int64_t timeUs = mBlockIter.blockTimeUs();
+    if (timeUs < 0) {
+        ALOGW("mBlockIter.blockTimeUs() is negative, hence exiting");
+        return UNKNOWN_ERROR;
+    }
     mBlockIter.advance();
     enum { VIDEO_TRACK = 1, AUDIO_TRACK = 2 };
     frameTimeUs = timeUs;
@@ -489,6 +493,10 @@ status_t MatroskaSource::readBlock() {
     if (track->GetType() == AUDIO_TRACK) {
         if (!mBlockIter.eos()) {
             nextBlkTimeUs = mBlockIter.blockTimeUs();
+            if (nextBlkTimeUs < 0) {
+                ALOGW("mBlockIter.blockTimeUs() is negative, hence exiting");
+                return UNKNOWN_ERROR;
+            }
             diffTimeUs = (nextBlkTimeUs - timeUs) / block->GetFrameCount();
         }
      }
