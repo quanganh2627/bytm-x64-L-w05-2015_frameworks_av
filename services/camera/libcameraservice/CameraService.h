94be8a35d58c3097998e936a70ff3a58fddc693c
Anttu Koski <anttu.koski@intel.com>
2013-10-08 13:43:52 +0300

    Revert "CameraServiceLayer: Add low priority instance support"
    BZ: 139717 143481
    
    This reverts commit 5005bf107bebf27829d1a77f438ac0948989628f.
    This reverts patch http://android.intel.com:8080/#/c/133116/
    
    Change-Id: Ia4fe7866d92c1e3fb5a36c24371b8aed689afafc
    Signed-off-by: Anttu Koski <anttu.koski@intel.com>
    Reviewed-on: http://android.intel.com:8080/136868
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -121,8 +121,6 @@ public:
             return mRemoteBinder;
         }
 
-        void serviceDisconnect();
-
     protected:
         BasicClient(const sp<CameraService>& cameraService,
                 const sp<IBinder>& remoteCallback,
@@ -225,9 +223,6 @@ public:
             return mRemoteCallback;
         }
 
-        bool                 isLowPriorityClient() { return mLowPriorityClient; }
-        void                 setLowPriority() { mLowPriorityClient = true; }
-
     protected:
         static Mutex*        getClientLockFromCookie(void* user);
         // convert client from cookie. Client lock should be acquired before getting Client.
@@ -239,11 +234,6 @@ public:
 
         // - The app-side Binder interface to receive callbacks from us
         sp<ICameraClient>               mRemoteCallback;
-
-        // When true, the client is running as a background activity and can be
-        // closed by the ServiceLayer when normal priority camera requests are
-        // started by the applications
-        bool                            mLowPriorityClient;
 
     }; // class Client
 
@@ -318,8 +308,6 @@ private:
     wp<Client>          mClient[MAX_CAMERAS];  // protected by mServiceLock
     Mutex               mClientLock[MAX_CAMERAS]; // prevent Client destruction inside callbacks
     int                 mNumberOfCameras;
-
-    int                 mFrontCameraId;
 
     typedef wp<ProClient> weak_pro_client_ptr;
     Vector<weak_pro_client_ptr> mProClientList[MAX_CAMERAS];
