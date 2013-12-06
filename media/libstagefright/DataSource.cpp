warpboard/platform_frameworks_av

libstagefright: enable AVI playback on JB
Change-Id: I2034c28badf9e1c3d01ed5e744a21846e73f3a3a
Author: Yi Wang <yi.a.wang@intel.com>
Signed-off-by: Yi Wang <yi.a.wang@intel.com>
Signed-off-by: Xiaolin Zhang <xiaolin.zhang@intel.com>
Singed-off-by: Shuo Gao <shuo.gao@intel.com>
Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
Signed-off-by: Jack Ren <jack.ren@intel.com>
Autho-tracking-BZ: 47452, 22678, 24731,41880

media/libstagefright/DataSource.cpp
@@ -19,6 +19,7 @@
 #if CHROMIUM_AVAILABLE
 #include "include/DataUriSource.h"
 #endif
+#include "include/AVIExtractor.h"
 
 #include "include/MP3Extractor.h"
 #include "include/MPEG4Extractor.h"
@@ -120,6 +121,7 @@ void DataSource::RegisterDefaultSniffers() {
     RegisterSniffer(SniffAAC);
     RegisterSniffer(SniffMPEG2PS);
     RegisterSniffer(SniffWVM);
+    RegisterSniffer(SniffAVI);
 
     char value[PROPERTY_VALUE_MAX];
     if (property_get("drm.service.enabled", value, NULL)

quanganh2627/bytm-x64-L-w05-2015_frameworks_av

BZ: 47452
original patch:
34232: libstagefright: enable avi playback on ICS
39311: libstagefright: skip user define chunk in index chunk in AVI Extractor
52722: framework: re-enable AVI for CTP
Category: aosp improvement
Domain: Video.Media-container
Origin: internal
Upstream-Candidate: yes
Change-Id: I7a7c589926444b7196f7d730e455a487dcb1f79a
Orig-Change-Id: I624fd0cc8a58811ee89f8fc807bce0b4ab0b0b80
Signed-off-by: ywan171 <yi.a.wang@intel.com>
Signed-off-by: Yuanjun Huang <yuanjun.huang@intel.com>

media/libstagefright/DataSource.cpp
@@ -19,6 +19,7 @@
 #if CHROMIUM_AVAILABLE
 #include "include/chromium_http_stub.h"
 #endif
+#include "include/AVIExtractor.h"
 
 #include "include/AACExtractor.h"
 #include "include/DRMExtractor.h"
@@ -169,6 +170,7 @@ void DataSource::RegisterDefaultSniffers() {
     RegisterSniffer_l(SniffAAC);
     RegisterSniffer_l(SniffMPEG2PS);
     RegisterSniffer_l(SniffWVM);
+    RegisterSniffer_l(SniffAVI);
 
     char value[PROPERTY_VALUE_MAX];
     if (property_get("drm.service.enabled", value, NULL)