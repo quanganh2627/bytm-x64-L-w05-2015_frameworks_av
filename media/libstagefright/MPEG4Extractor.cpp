quanganh2627/bytm-x64-L-w05-2015_frameworks_av

commit db5dbc6b21609f278b9688d01a42741ca7485571

MPEG4Extractor: add support for qt legacy media files
BZ: 164599

The test clip has two issues:
1) the 'udta' Atom in audio/video track is empty and should be skipped.
2) the audio track's esds is nested in 'wave' Atom which is contained in 'mp4a' Atom.

Besides, when verifyTrack() fails, this track should be removed from track list and
parsing needs to be continued.

Change-Id: I95727d7e8c450498816288bf12be6b53d0a48289
Category: aosp improvement
Domain: Video.Media-container
Origin: internal
Upstream-Candidate: no, proprietary
Signed-off-by: Dan Liang <dan.liang@intel.com>

media/libstagefright/MPEG4Extractor.cpp
@@ -818,6 +818,12 @@ status_t MPEG4Extractor::parseUdtaMetaData(off64_t offset, size_t size) {
             chunk_type = U32_AT((uint8_t*)&buffer[buffer_off + 4]);
 
             ALOGV("chunk_size = %d", chunk_size);
+            if (!chunk_size) {
+                ALOGI("Invalid chunk length.");
+                delete[] buffer;
+                buffer = NULL;
+                return ERROR_MALFORMED;
+            }
 
             uint32_t metadataKey = 0;
             switch (chunk_type) {
@@ -986,23 +992,25 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
                 track->meta->setCString(kKeyMIMEType, "application/octet-stream");
             }
 
+            off64_t stop_offset = *offset + chunk_size;
+
             if (chunk_type == FOURCC('u', 'd', 't', 'a')) {
                 parseUdtaMetaData(data_offset, chunk_data_size);
-            }
-
-            off64_t stop_offset = *offset + chunk_size;
-            *offset = data_offset;
-            while (*offset < stop_offset) {
-                status_t err = parseChunk(offset, depth + 1);
-                if (err != OK) {
-                    if (chunk_type == FOURCC('t', 'r', 'a', 'k')){
-                        // If one 'trak' box contains error, we can skip it to keep parsing,
-                        // which make sure we can parse out following valid 'trak' in the clip
-                        ALOGE("invalid track, skip it and keep parsing");
-                        mLastTrack->skipTrack = true;
-                        *offset = stop_offset;
-                    } else {
-                        return err;
+                *offset += chunk_size;
+            } else {
+                *offset = data_offset;
+                while (*offset < stop_offset) {
+                    status_t err = parseChunk(offset, depth + 1);
+                    if (err != OK) {
+                        if (chunk_type == FOURCC('t', 'r', 'a', 'k')){
+                            // If one 'trak' box contains error, we can skip it to keep parsing,
+                            // which make sure we can parse out following valid 'trak' in the clip
+                            ALOGE("invalid track, skip it and keep parsing");
+                            mLastTrack->skipTrack = true;
+                            *offset = stop_offset;
+                        } else {
+                            return err;
+                        }
                     }
                 }
             }
@@ -1012,8 +1020,12 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             }
 
             if (isTrack) {
-                if (mLastTrack->skipTrack) {
+                if (mLastTrack->skipTrack || OK != verifyTrack(mLastTrack)) {
                     Track *cur = mFirstTrack;
+
+                    const char *mime;
+                    CHECK(mLastTrack->meta->findCString(kKeyMIMEType, &mime));
+                    ALOGI("%s track is removed", !strncasecmp("video/", mime, 6) ? "video" : "audio");
 
                     if (cur == mLastTrack) {
                         delete cur;
@@ -1028,12 +1040,6 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
                     }
 
                     return OK;
-                }
-
-                status_t err = verifyTrack(mLastTrack);
-
-                if (err != OK) {
-                    return err;
                 }
             } else if (chunk_type == FOURCC('m', 'o', 'o', 'v')) {
                 mInitCheck = OK;
@@ -1466,9 +1472,12 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
 #endif //DOLBY_UDC
         {
             uint8_t buffer[8 + 20];
-            if (chunk_data_size < (ssize_t)sizeof(buffer)) {
-                // Basic AudioSampleEntry size.
-                return ERROR_MALFORMED;
+
+            // Some special clips contains more than one 'mp4a' box and the later one is invalid
+            int32_t num = 0xff;
+            if (mLastTrack->meta->findInt32(kKeyChannelCount, &num) && num <= 2) {
+                *offset += chunk_size;
+                break;
             }
 
             if (mDataSource->readAt(
@@ -1498,7 +1507,16 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             mLastTrack->meta->setInt32(kKeySampleRate, sample_rate);
 
             off64_t stop_offset = *offset + chunk_size;
-            *offset = data_offset + sizeof(buffer);
+            if (chunk_type == FOURCC('m', 'p', '4', 'a')) {
+                int32_t next_chunk;
+                mDataSource->readAt(data_offset + 48, &next_chunk, 4);
+                next_chunk = ntohl(next_chunk);
+                if (next_chunk == FOURCC('w', 'a', 'v', 'e'))
+                    *offset = data_offset + 44;
+                else
+                    *offset = data_offset + 28;
+            }
+
             while (*offset < stop_offset) {
                 status_t err = parseChunk(offset, depth + 1);
                 if (err != OK) {
@@ -2049,6 +2067,23 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             parseSegmentIndex(data_offset, chunk_data_size);
             *offset += chunk_size;
             return UNKNOWN_ERROR; // stop parsing after sidx
+        }
+
+        case FOURCC('w', 'a', 'v', 'e'):
+        {
+            off64_t stop_offset = *offset + chunk_size;
+            *offset = data_offset;
+            while (*offset < stop_offset) {
+                status_t err = parseChunk(offset, depth + 1);
+                if (err != OK) {
+                    return err;
+                }
+            }
+
+            if (*offset != stop_offset) {
+                return ERROR_MALFORMED;
+            }
+            break;
         }
 
         default:

commit c6900f5298f066a335aba81cdeb9003492fc8f2f

MPEG4Extractor: fix a regression caused by commit db5dbc6b
BZ: 164599

Only update the offset by adding 44 when the atom 'wave' is found.

Change-Id: I0f4110e6d3dccc073f108934fd20c2df3e2e26b1
Category: aosp improvement
Domain: Video.Media-container
Origin: internal
Upstream-Candidate: no, proprietary
Signed-off-by: Dan Liang <dan.liang@intel.com>

media/libstagefright/MPEG4Extractor.cpp
@@ -1507,14 +1507,14 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             mLastTrack->meta->setInt32(kKeySampleRate, sample_rate);
 
             off64_t stop_offset = *offset + chunk_size;
+            *offset = data_offset + sizeof(buffer);
+
             if (chunk_type == FOURCC('m', 'p', '4', 'a')) {
                 int32_t next_chunk;
                 mDataSource->readAt(data_offset + 48, &next_chunk, 4);
                 next_chunk = ntohl(next_chunk);
                 if (next_chunk == FOURCC('w', 'a', 'v', 'e'))
                     *offset = data_offset + 44;
-                else
-                    *offset = data_offset + 28;
             }
 
             while (*offset < stop_offset) {