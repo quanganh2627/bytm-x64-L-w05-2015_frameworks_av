f847202a525d957009639c62794aa4b2b5c8246f
jpadmana <jayashree.r.padmanaban@intel.com>
2014-04-12 18:08:15 +0530

    [Music offload] Sound is lost on IHF when resuming mp3 after BT headset is disconnected
    BZ: 186495
    
    The issue is because of  limitation with the implementation of Condition
    variable in AsyncCallbackThread.
    Its a timing issue. The signal sent to this Async thread is lost because
    the thread is not waiting during signalling. Once the signal is lost,
    the thread gets into a wait forever, blocking further execution.
    A variable check is added to find if the thread is signaled during this
    processing (i.e not waiting for signal state) before it waits for signal
    again
    
    Change-Id: Id53c5050b1abecc88fec7059b0bc413516bfe473
    Category: aosp improvement
    Domain: AUDIO_MEDIA-Audio Flinger
    Origin: upstream https://android-review.googlesource.com/#/c/72782/
    Upstream-Candidate: no, proprietary
    Signed-off-by: jpadmana <jayashree.r.padmanaban@intel.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4063,7 +4063,11 @@ bool AudioFlinger::AsyncCallbackThread::threadLoop()
 
         {
             Mutex::Autolock _l(mLock);
-            mWaitWorkCV.wait(mLock);
+            while (!((mWriteAckSequence & 1) ||
+                     (mDrainSequence & 1) ||
+                     exitPending())) {
+                mWaitWorkCV.wait(mLock);
+            }
             if (exitPending()) {
                 break;
             }
